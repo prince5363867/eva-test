3c548277697311412810002e7e8bf45215531758
Young <369221189@qq.com>
2019-09-05 10:18:51 +0800

    #到此一游哎~

--- src/main/java/com/eva/evaTest/TestApplication.java
+++ src/main/java/com/eva/evaTest/TestApplication.java
@@ -10,6 +10,7 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args) ;
+		System.out.println("☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺ STARTED ☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺");
 	}
 
 }
