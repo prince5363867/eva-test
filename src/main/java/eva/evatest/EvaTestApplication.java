c5a534d839c3e7818cf6a3b823d332e9e9f85582
fangyinjian <yinjian.fang@hpe-airchina.com>
2019-09-15 21:33:41 +0800

    重构项目架构

---
+++ src/main/java/eva/evatest/EvaTestApplication.java
@@ -0,0 +1,13 @@
+package eva.evatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaTestApplication.class, args);
+    }
+
+}
